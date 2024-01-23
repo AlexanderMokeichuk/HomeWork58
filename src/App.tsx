30dc5fd5d3b431f55fba285f632111539e0e585d
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-01-23 12:34:56 +0600

    Wrote Modal and Backdrop components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import Modal from "./comronents/Modal/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {useState} from "react";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
 
+  const cancel = () => setShowModal(false);
   return (
-    <>
+    <div className={"container-fluid mt-5 d-flex "} style={{width: 1000, height: 700}}>
+      <div className={"w-75 d-flex align-items-center"}>
+        <Modal
+          show={showModal}
+          title="Modal"
+          onClose={cancel}
+        >
+          <div className="modal-body">
+            Content
+          </div>
+          <div className="modal-footer">
+            <button
+              className="btn btn-danger"
+              onClick={cancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </Modal>
 
-    </>
+        <button
+          className="btn btn-primary"
+          onClick={() => setShowModal(true)}
+        >
+          Show modal window
+        </button>
+      </div>
+      <div className={"w-100 h-100 rounded-2 border border-info overflow-auto"}>
+
+      </div>
+    </div>
   );
 }
 
